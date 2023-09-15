70bade8ac223f9ee8e881c287618cd7d4434e08e
shalenMathew <shalenmj@gmail.com>
2023-09-15 16:20:34 +0530

    toast commit

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,16 +3,16 @@ package com.example.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        Toast.makeText(this, "", Toast.LENGTH_SHORT).show()
 
     }
 
-    private fun demoAccount() {
 
-    }
 }
