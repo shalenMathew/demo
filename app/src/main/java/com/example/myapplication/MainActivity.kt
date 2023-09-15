f42bd8942b78b1a27ab7cd74ce9d51c9f4c364e9
shalenMathew <shalenmj@gmail.com>
2023-09-15 16:02:25 +0530

    commit 1

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        demoAccount()
+    }
+
+    private fun demoAccount() {
+        Log.d("TAG","Hello")
+    }
+}
